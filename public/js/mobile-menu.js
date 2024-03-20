a4f19994967f251b251a35bf3ca180c4e149e7c5
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2024-03-20 08:23:27 +0100

    style: navbar mobile menu fix

--- public/js/mobile-menu.js
+++ public/js/mobile-menu.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 export function mobileMenu() {
   let block = document.querySelector("header.header");
 
@@ -10,6 +12,23 @@ export function mobileMenu() {
       hamburger.classList.toggle("is-active");
       body.classList.toggle("scroll-lock");
       mobileMenu.classList.toggle("active");
+      block.classList.toggle("mobile-menu-active");
+
+      if (mobileMenu.classList.contains("active")) {
+        animateContent();
+      }
     });
+
+    function animateContent() {
+      const menuItems = mobileMenu.querySelectorAll(".menu-item");
+      const authLinks = mobileMenu.querySelectorAll(".auth-links a");
+
+      let tl = gsap.timeline({});
+
+      tl.from(menuItems, { autoAlpha: 0, y: 50, stagger: 0.3 }, ">=0.5").from(
+        authLinks,
+        { autoAlpha: 0, y: 20, stagger: 0.2 }
+      );
+    }
   }
 }
